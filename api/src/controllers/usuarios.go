653ba943a8b781bf9ec7ecfaf1d8561b9a5c4b6c
Rick Farias <167890284+Rickfarias@users.noreply.github.com>
2024-07-08 08:51:59 -0300

    att

---
+++ api/src/controllers/usuarios.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando Usuário!"))
+}
+
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando todos os usuários)!"))
+}
+
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando um Usuário!"))
+}
+
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Atualizando um usuário!"))
+}
+
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deletando um usuário!"))
+}
