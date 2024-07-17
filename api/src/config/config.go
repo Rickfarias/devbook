08a04ceec620c265dbdf2c6cdf367eb3e6d06c2d
Rick Farias <167890284+Rickfarias@users.noreply.github.com>
2024-07-17 15:13:46 -0300

    token de auth e middlewares

--- api/src/config/config.go
+++ api/src/config/config.go
@@ -15,6 +15,9 @@ var (
 
 	// Porta onde a API vai estar rodando
 	Porta = 0
+
+	// SecrectKey é a chave que será utilizada para assinar o token
+	SecretKey []byte
 )
 
 // carregar vai inicializar as variáveis de ambiente
@@ -35,4 +38,6 @@ func Carregar() {
 		os.Getenv("DB_SENHA"),
 		os.Getenv("DB_NOME"),
 	)
+
+	SecretKey = []byte(os.Getenv("SECRET_KEY"))
 }
